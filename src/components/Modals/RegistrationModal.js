80e763a217c9b95a65d249a5983fca3af9437e92
Artem Lazuk <artem.l@datarails.com>
2022-01-07 13:17:59 +0300

    feature: add regex for fields

--- src/components/Modals/RegistrationModal.js
+++ src/components/Modals/RegistrationModal.js
@@ -1,4 +1,4 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import AuthService from "../../services/auth";
 import GoogleLogin from "react-google-login";
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
@@ -9,35 +9,58 @@ import * as action from "../../store/actions/user"
 import {useDispatch} from "react-redux";
 
 function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPasswordModal}) {
-    const dispatch = useDispatch();
     const emailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    const loginRegex = /^[\w]+$/;
+    const passwordRegex = /^(?=.*\d)[0-9a-zA-Z]{6,}$/;
+
+    const dispatch = useDispatch();
+    const generalErrorMessage = useRef();
+
+    const emailInputRef = useRef();
+    const loginInputRef = useRef();
+    const passwordInputRef = useRef();
 
     const [userLogin, setUserLogin] = useState('');
     const [userPassword, setUserPassword] = useState('');
     const [userEmail, setUserEmail] = useState('');
-    const [dataError, setDataError] = useState();
+    const [inputFields, setInputFields] = useState([]);
 
-    const generalErrorMessage = useRef();
+    useEffect(() => {
+        setInputFields([
+            {target: emailInputRef.current, state: 'email'},
+            {target: loginInputRef.current, state: 'login'},
+            {target: passwordInputRef.current, state: 'password'}
+        ])
+    }, [])
 
     function showLoginModal () {
         showRegistrationModal(false);
         showResetPasswordModal(false)
     }
 
+    function displayError (msg) {
+        if(!msg) return generalErrorMessage.current.classList.remove('active');
+
+        generalErrorMessage.current.textContent = msg;
+        generalErrorMessage.current.classList.add('active');
+    }
+
     function onRegistrationClick (evt) {
         evt.preventDefault();
-        console.log(generalErrorMessage)
-        generalErrorMessage.current.classList.remove('active');
+        displayError(false);
+
+        inputFields.forEach((field) => checkInputData(field.target, field.state))
+
+        if (inputFields.length) return;
 
         AuthService.registration(userLogin, userPassword, userEmail)
             .then((response) => {
                 if(response.data.isSuccessful) {
-                    localStorage.setItem('token', response.data.token)
+                    localStorage.setItem('token', response.data.token);
                     dispatch(action.changeUserAuntification(true));
                     onCloseModal();
                 } else {
-                    generalErrorMessage.current.textContent = 'Данный аккаунт уже создан!';
-                    generalErrorMessage.current.classList.add('active');
+                    displayError('Данный аккаунт уже создан!');
                 }
             })
             .catch((error) => {
@@ -45,24 +68,73 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
             })
     }
 
-    function OnInputChange (target, state) {
+    function socialRegistationClick (response, type) {
+        displayError(false);
+
+        const setUserAuthorize = (response) => {
+            displayError(false);
+
+            if (response.data.isSuccessful) {
+                localStorage.setItem('token', response.data.token);
+                dispatch(action.changeUserAuntification(true));
+                onCloseModal();
+            } else {
+                displayError('Произошла ошибка регистрации');
+            }
+        }
+
+        switch (type) {
+            case 'google':
+                AuthService.registrationByGoogle(response)
+                    .then(async (response) => {
+                        setUserAuthorize(response);
+                    })
+                    .catch((error) => {
+                        console.log('registation by google failed', error);
+                        displayError('Произошла ошибка регистрации через сервис google');
+                    })
+                break;
+
+            case 'facebook':
+                AuthService.registrationByFacebook(response)
+                    .then(async (response) => {
+                        setUserAuthorize(response);
+                    })
+                    .catch((error) => {
+                        console.log('registation by facebook failed', error);
+                        displayError('Произошла ошибка регистрации через сервис facebook');
+                    })
+                break;
+        }
+    }
+
+    function checkInputData (target, state) {
         const outputErrorEl = target.nextElementSibling;
 
         const addError = (input, underInput, errorText) => {
             input.classList.add('form__input__error');
             underInput.classList.add('active');
             underInput.textContent = errorText;
-        }
+
+            if(inputFields.find((item) => item.state === state)) return;
+            setInputFields((prev) => [...prev, {target, state}])
+        };
 
         const removeError = (input, underInput, errorText) => {
             input.classList.remove('form__input__error');
             underInput.classList.remove('active');
             underInput.textContent = errorText;
-        }
+
+            setInputFields((prev) => prev.filter((field) => (field.target !== target)))
+        };
 
         if (state === 'email') {
-            if (target.value <= 0 || (target.value.search(emailRegex) === -1)) {
-                addError(target, outputErrorEl, 'Почта введена некорректно');
+            if (target.value.length <= 0) {
+                addError(target, outputErrorEl, 'Вы не указали почтовый ящик');
+            } else if (target.value.length < 10 || target.value.length > 128) {
+                addError(target, outputErrorEl, 'Длинна почтового ящика должна быть более 10 и менее 128 символов');
+            } else if (target.value.search(emailRegex) === -1) {
+                addError(target, outputErrorEl, 'Почтовый ящик введен некорректно');
             } else {
                 removeError(target, outputErrorEl, '')
             }
@@ -72,14 +144,38 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
         }
 
         if (state === 'login') {
+            if (target.value.length <= 0) {
+                addError(target, outputErrorEl, 'Вы не указали логин');
+            } else if (target.value.length < 4 || target.value.length > 16) {
+                addError(target, outputErrorEl, 'Длинна логина должна быть более 4 и менее 16 символов');
+            } else if (target.value.search(loginRegex) === -1) {
+                addError(target, outputErrorEl, 'Логин указан некорректно');
+            } else {
+                removeError(target, outputErrorEl, '')
+            }
+
             setUserLogin(target.value.trim())
             return;
         }
 
         if (state === 'password') {
+            if (target.value.length <= 0) {
+                addError(target, outputErrorEl, 'Вы не указали пароль');
+            } else if (target.value.length < 6 || target.value.length > 32) {
+                addError(target, outputErrorEl, 'Длинна логина должна быть более 6 и менее 32 символов');
+            } else if (target.value.search(passwordRegex) === -1) {
+                addError(target, outputErrorEl, 'Пароль должен содержать хотябы одну цифру');
+            } else {
+                removeError(target, outputErrorEl, '')
+            }
+
             setUserPassword(target.value.trim())
             return;
         }
+    }
+
+    function OnInputChange (target, state) {
+        checkInputData(target, state);
     }
 
     return (
@@ -95,8 +191,8 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
                                 render={renderProps => (
                                     <img onClick={renderProps.onClick} src="/img/social/google.svg" alt="google" width="50" height="50"/>
                                 )}
-                                onSuccess={AuthService.registrationByGoogle}
-                                onFailure={AuthService.registrationByGoogle}
+                                onSuccess={(response) => socialRegistationClick(response, 'google')}
+                                onFailure={(response) => socialRegistationClick(response, 'google')}
                                 autoLoad={false}
                             />
                         </li>
@@ -104,7 +200,7 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
                             <FacebookLogin
                                 appId="1107954079941584"
                                 redirectUri="https://ca34-95-10-3-29.ngrok.io/api/User/facebook"
-                                callback={AuthService.registrationByFacebook}
+                                callback={(response) => socialRegistationClick(response, 'facebook')}
                                 cssClass={''}
                                 textButton={''}
                                 render={renderProps => (
@@ -148,6 +244,7 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
                     <form className="form">
                         <p className="form__input-wrapper">
                             <input
+                                ref={emailInputRef}
                                 className="form__input"
                                 type="text"
                                 placeholder="Почта"
@@ -158,6 +255,7 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
                         </p>
                         <p className="form__input-wrapper">
                             <input
+                                ref={loginInputRef}
                                 className="form__input"
                                 type="text"
                                 placeholder="Имя пользователя"
@@ -168,6 +266,7 @@ function RegistrationModal ({onCloseModal, showRegistrationModal, showResetPassw
                         </p>
                         <p className="form__input-wrapper">
                             <input
+                                ref={passwordInputRef}
                                 className="form__input"
                                 type="password"
                                 placeholder="Пароль"
