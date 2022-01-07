148f266879b4dbb096fe12f1a31c2afca1318aca
Artem Lazuk <artem.l@datarails.com>
2022-01-07 13:10:25 +0300

    feature: improve app routing

--- src/AppRouting.js
+++ src/AppRouting.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import Home from "./pages/Home/Home"
 import NotFound from "./pages/NotFound";
+import AboutFilm from "./pages/AboutFilm/AboutFilm";
 
 function AppRouting () {
     return (
@@ -10,6 +11,7 @@ function AppRouting () {
                 <Route exact path="/home">
                     <Redirect to="/" />
                 </Route>
+                <Route path="/about_film/:id" component={AboutFilm}></Route>
                 <Route component={NotFound}></Route>
             </Switch>
             {/*<Route path="/:lang" component={SwitchLang}/>*/}
