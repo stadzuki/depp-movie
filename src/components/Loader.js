1f3979e21616117ebd7de9b79167af5e8d3982b6
Artem Lazuk <artem.l@datarails.com>
2022-01-09 21:01:11 +0300

    feature: add loader and dp-button styles

---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+function Loader () {
+    return (
+        <div className="loader-wrapper">
+            <div className="overlay"></div>
+            <div className="loader">
+                <img src="/img/loader.svg" alt="loading..." width="200" height="200"/>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
